653eca6fc27b692c668d96141dc4623f4877b587
sabarish1716 <developerformyself@gmail.com>
2025-03-08 12:36:47 +0530

    test,testDetails_creation

---
+++ routes/test.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const router = express.Router();
+const { pool } = require("../db");
+require("dotenv").config();
+
+
+router.post("/test", async (req, res) => {
+    try{
+        const { name, type, syrum, routienTime } = req.body;
+        if (!name || !type || !syrum || !routienTime) {
+            return res.status(400).json({ error: "All fields are required" });
+        }
+        const insertTestQuery = `
+            INSERT INTO public.test (name, type, syrum, routienTime) 
+            VALUES ($1, $2, $3, $4) RETURNING id;
+        `;
+        await pool.query(insertTestQuery, [name, type, syrum, routienTime]);
+        res.status(201).json({ success: true, message: "Test created!" });    }
+catch(e){
+    res.status(500).json({ error: "Test failed", details: e.message });
+}
+});
+
+router.get("/test", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT id, name, type, syrum, routienTime FROM public.test");
+        res.json({ success: true, test: result.rows });
+    } catch (error) {
+        console.error("Error fetching test:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+})
+
+router.get("/test/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const result = await pool.query("SELECT id, name, type, syrum, routienTime FROM public.test WHERE id = $1", [id]);
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: "Test not found" });
+        }
+        res.json({ success: true, test: result.rows });
+    } catch (error) {
+        console.error("Error fetching test:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+})
+
+
+router.delete("/test/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const result = await pool.query("DELETE FROM public.test WHERE id = $1", [id]);
+        if (result.rowCount === 0) {
+            return res.status(404).json({ error: "Test not found" });
+        }
+        res.json({ success: true, message: "Test deleted" });
+    } catch (error) {
+        console.error("Error deleting test:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+})
+
+router.put("/test/:id", async (req, res) => {
+    const { id } = req.params;
+    const { name, type, syrum, routienTime } = req.body;
+    try {
+        const updateTestQuery = `
+            UPDATE public.test
+            SET name = $1, type = $2, syrum = $3, routienTime = $4
+            WHERE id = $5;
+        `;
+        await pool.query(updateTestQuery, [name, type, syrum, routienTime, id]);
+        res.json({ success: true, message: "Test updated!" });
+    } catch (error) {
+        console.error("Error updating test:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+})
+
+
+
+module.exports = router;
---
+++ routes/testDetail.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { pool } = require("../db");
+require("dotenv").config();
+
+
+router.post("/testDetails", async (req, res) => {
+    try{
+        const { userId, testId, metrics, nextVisit, disaseId } = req.body;
+        if (!userId || !testId || !metrics  || !disaseId) {
+            return res.status(400).json({ error: "All fields are required" });
+        }
+        const insertTestDetailsQuery = `
+            INSERT INTO public.testDetails (userId, testId, metrics,  disaseId) 
+            VALUES ($1, $2, $3, $4, $5) RETURNING id;
+        `;
+        await pool.query(insertTestDetailsQuery, [userId, testId, metrics, nextVisit, disaseId]);
+        res.status(201).json({ success: true, message: "Test Details created!" });    }
+catch(e){
+    res.status(500).json({ error: "Test Details failed", details: e.message });
+}
+});
+
+router.get("/testDetails", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT id, userId, testId, metrics, nextVisit, disaseId FROM public.testDetails");
+        res.json({ success: true, testDetails: result.rows });
+    } catch (error) {
+        console.error("Error fetching testDetails:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+router.get("/testDetails/:userId", async (req, res) => {
+    const { userId } = req.params;
+    try {
+        const result = await pool.query("SELECT id, userId, testId, metrics, nextVisit, disaseId FROM public.testDetails WHERE userId = $1", [userId]);
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: "Test Details not found" });
+        }
+        res.json({ success: true, testDetails: result.rows });
+    } catch (error) {
+        console.error("Error fetching testDetails:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+})
+
+router.put("/testDetails/:id", async (req, res) => {
+    const { id } = req.params;
+    const { userId, testId, metrics, nextVisit, disaseId } = req.body;
+    try {
+        const updateTestDetailsQuery = `
+            UPDATE public.testDetails
+            SET userId = $1, testId = $2, metrics = $3, nextVisit = $4, disaseId = $5
+            WHERE id = $6;
+        `;
+        await pool.query(updateTestDetailsQuery, [userId, testId, metrics, nextVisit, disaseId, id]);
+        res.json({ success: true, message: "Test Details updated!" });
+    } catch (error) {
+        console.error("Error updating testDetails:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+})
+
+router.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deleteTestDetailsQuery = `
+            DELETE FROM public.testDetails
+            WHERE id = $1;
+        `;
+        await pool.query(deleteTestDetailsQuery, [id]);
+        res.json({ success: true, message: "Test Details deleted!" });
+    } catch (error) {
+        console.error("Error deleting testDetails:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+})
+
+
+
+
+
+module.exports = router;
