051f495c07729b39b63d28d1f6db32c18f999748
SatheeshKumar <satheezmech95@gmail.com>
2025-03-08 14:35:15 +0530

    role crud

---
+++ routes/role.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const router = express.Router();
+const { pool } = require("../db");
+require("dotenv").config();
+
+
+router.post("/role", async (req, res) => {
+    try{
+        const { name } = req.body;
+        if (!name) {
+            return res.status(400).json({ error: "name is required" });
+        }
+        const insertTestQuery = `
+            INSERT INTO public.test (name) 
+            VALUES ($1) RETURNING id;
+        `;
+        await pool.query(insertTestQuery, [name]);
+        res.status(201).json({ success: true, message: "Role created!" });    }
+catch(e){
+    res.status(500).json({ error: "Role failed", details: e.message });
+}
+});
+
+router.get("/role", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT name FROM public.test");
+        res.json({ success: true, role: result.rows });
+    } catch (error) {
+        console.error("Error fetching test:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+})
+
+router.get("/role/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const result = await pool.query(" name FROM public.test WHERE id = $1", [id]);
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: "Role not found" });
+        }
+        res.json({ success: true, role: result.rows });
+    } catch (error) {
+        console.error("Error fetching test:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+})
+
+
+router.delete("/role/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const result = await pool.query("DELETE FROM public.test WHERE id = $1", [id]);
+        if (result.rowCount === 0) {
+            return res.status(404).json({ error: "Role not found" });
+        }
+        res.json({ success: true, message: "Role deleted" });
+    } catch (error) {
+        console.error("Error deleting role:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+})
+
+router.put("/role/:id", async (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    try {
+        const updateTestQuery = `
+            UPDATE public.test
+            SET name = $1,
+            WHERE id = $2;
+        `;
+        await pool.query(updateTestQuery, [name,id]);
+        res.json({ success: true, message: "Role updated!" });
+    } catch (error) {
+        console.error("Error updating role:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+})
+
+
+
+module.exports = router;
